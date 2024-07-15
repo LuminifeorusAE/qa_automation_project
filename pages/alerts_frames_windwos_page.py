f41298ff904d73e3cc190ffa30170ea13052eabf
David <dtadevosyan53@gmail.com>
2024-07-15 13:18:38 +0400

    optimized method in alerts_frames_windwos_page.py

--- pages/alerts_frames_windwos_page.py
+++ pages/alerts_frames_windwos_page.py
@@ -9,19 +9,17 @@ class BrowserWindowsPage(BasePage):
     locators = BrowserWindowsPageLocators()
 
     def check_new_tab_and_window(self):
-        # Check new tab
-        self.element_is_clickable(self.locators.NEW_TAB_BUTTON).click()
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        text_title_tab = self.element_present(self.locators.SAMPLE_TEXT_TITLE).text
-        self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[0])
+        buttons = [self.locators.NEW_TAB_BUTTON, self.locators.NEW_WINDOW_BUTTON]
+        titles = []
 
-        self.element_is_clickable(self.locators.NEW_WINDOW_BUTTON).click()
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        text_title_window = self.element_present(self.locators.SAMPLE_TEXT_TITLE).text
-        self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[0])
+        for button in buttons:
+            self.element_is_clickable(button).click()
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            titles.append(self.element_present(self.locators.SAMPLE_TEXT_TITLE).text)
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
 
-        return text_title_tab, text_title_window
+        return titles[0], titles[1]
+
 
 
