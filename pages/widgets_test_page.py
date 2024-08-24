b73fc1e315bbe09063ce4ba13780561a5a8f5613
David <dtadevosyan53@gmail.com>
2024-08-24 10:29:55 +0400

    added documentations

--- pages/widgets_test_page.py
+++ pages/widgets_test_page.py
@@ -18,6 +18,15 @@ class AccordianPage(BasePage):
 
     @allure.step("test check accordian")
     def check_accordian(self, accordian_num):
+        """
+        Checks the accordion section (either 'first', 'second', or 'last'), clicks on the title,
+        and retrieves the content length and the section title text.
+
+        Args:
+            accordian_num (str): Specifies which accordion section to interact with ('first', 'second', 'last').
+            Returns:
+            list: The title of the section and the length of the content text.
+        """
         accordian = {
             'first': {
                 'title': self.locators.FIRST_SECTION,
@@ -32,11 +41,11 @@ class AccordianPage(BasePage):
                 'content': self.locators.LAST_SECTION_CONTENT
             }
         }
-
+        # Click on the section title and retrieve the content length
         section_title = self.element_is_clickable(accordian[accordian_num]['title'])
         self.go_to_element(section_title)
         section_title.click()
-
+        # Retry clicking if TimeoutException is raised.
         try:
             section_content = self.visible_element(accordian[accordian_num]['content']).text
         except TimeoutException:
@@ -52,6 +61,12 @@ class AutoCompletePage(BasePage):
 
     @allure.step("test fill multi input line ")
     def fill_multi_input(self):
+        """
+        Fills the multi-input line with random colors using auto-complete feature.
+
+       Returns:
+            list: The list of colors that were entered.
+       """
         colors = random.sample(next(generated_color()).color_name, k=random.randint(2, 5))
         for color in colors:
             input_multi = self.element_is_clickable(self.locators.MULTI_INPUT)
@@ -61,6 +76,12 @@ class AutoCompletePage(BasePage):
 
     @allure.step("test removing value in multi input line ")
     def remove_value_from_multi(self):
+        """
+        Removes a single value from the multi-input line.
+
+        Returns:
+            tuple: Count of values before and after removing one.
+        """
         count_value_before = len(self.elements_are_present(self.locators.MULTI_INPUT_VALUE))
         remove_button_list = self.every_visible_element(self.locators.MULTI_VALUE_REMOVE)
         for value in remove_button_list:
@@ -73,6 +94,12 @@ class AutoCompletePage(BasePage):
 
     @allure.step("test check color in multi")
     def check_color_in_multi(self):
+        """
+        Retrieves the colors currently present in the multi-input container.
+
+        Returns:
+            list: List of colors currently selected in the multi-input.
+        """
         color_list = self.elements_are_present(self.locators.MULTI_INPUT_VALUE)
         colors = []
         for color in color_list:
@@ -81,6 +108,12 @@ class AutoCompletePage(BasePage):
 
     @allure.step("fill single input")
     def fill_single_input(self):
+        """
+        Fills the single-input line with a random color using the auto-complete feature.
+
+        Returns:
+            str: The color that was entered.
+        """
         color = random.sample(next(generated_color()).color_name, k=1)
         input_single = self.element_is_clickable(self.locators.SINGLE_INPUT)
         input_single.send_keys(color)
@@ -89,6 +122,12 @@ class AutoCompletePage(BasePage):
 
     @allure.step("check color in single container")
     def check_color_in_single(self):
+        """
+        Retrieves the color currently present in the single-input container.
+
+        Returns:
+            str: The color currently selected in the single-input.
+        """
         color = self.visible_element(self.locators.SINGLE_CONTAINER)
         return color.text
 
@@ -98,6 +137,12 @@ class DatePickerPage(BasePage):
 
     @allure.step("test selecting date")
     def select_date(self):
+        """
+        Selects a date using the date picker by setting the month, year, and day.
+
+        Returns:
+            tuple: The value of the date before and after the selection.
+        """
         date = next(generate_date())
         input_date = self.visible_element(self.locators.DATE_INPUT)
         value_date_before = input_date.get_attribute('value')
@@ -111,6 +156,12 @@ class DatePickerPage(BasePage):
 
     @allure.step("test select date and time ")
     def select_date_and_time(self):
+        """
+        Selects both date and time using the date-time picker.
+
+        Returns:
+            tuple: The value of the date-time before and after the selection.
+        """
         date = next(generate_date())
         input_date = self.visible_element(self.locators.DATE_AND_TIME_INPUT)
         value_date_before = input_date.get_attribute('value')
@@ -127,11 +178,25 @@ class DatePickerPage(BasePage):
 
     @allure.step("test set date by text")
     def set_date_by_text(self, element, value):
+        """
+        Selects an option from a drop-down based on the visible text.
+
+        Args:
+            element: The element of the drop-down to select from.
+            value (str): The visible text of the option to select.
+        """
         select = Select(self.element_present(element))  # !!!add Select methods in base page later !!!
         select.select_by_visible_text(value)
 
     @allure.step("test date item from list ")
     def set_date_item_from_list(self, elements, value):
+        """
+        Selects a specific item from a list of elements based on its text.
+
+        Args:
+            elements: The list of elements to choose from.
+            value (str): The text value to match and click on.
+        """
         item_list = self.elements_are_present(elements)
         for item in item_list:
             if item.text == value:
@@ -144,6 +209,12 @@ class SliderPage(BasePage):
 
     @allure.step("test change slider value")
     def change_slider_value(self):
+        """
+        Adjusts the slider by a random offset and retrieves its value before and after the change.
+
+        Returns:
+            tuple: The slider value before and after changing it.
+        """
         value_before = self.visible_element(self.locators.SLIDER_VALUE).get_attribute('value')
         slider_input = self.visible_element(self.locators.INPUT_SLIDER)
         self.action_drag_and_drop_by_offset(slider_input, random.randint(1, 100), 0)
@@ -157,6 +228,12 @@ class ProgressBarPage(BasePage):
     @allure.step("test change progress bar value ")
     #  write method that waits until progress bar is finished and resets it
     def change_progress_bar_value(self):
+        """
+        Starts the progress bar, waits for a random amount of time, then stops it.
+
+        Returns:
+            tuple: The progress value before and after the operation.
+        """
         value_before = self.not_visible_elements(self.locators.PROGRESS_VALUE).get_attribute('aria-valuenow')
         progress_bar_button = self.element_is_clickable(self.locators.START_BUTTON)
         progress_bar_button.click()
@@ -171,6 +248,15 @@ class TabsPage(BasePage):
 
     @allure.step("test check tabs contents")
     def check_tabs(self, tabs_name):
+        """
+        Checks the contents of a specific tab by its name ('what', 'origin', 'use', 'more').
+
+        Args:
+            tabs_name (str): The name of the tab to select.
+
+        Returns:
+            tuple: The tab's title and the length of the content text.
+        """
         tabs = {
             'what': {
                 'title': self.locators.WHAT_TAB,
@@ -202,6 +288,16 @@ class ToolTipsPage(BasePage):
 
     @allure.step("get text from tool tips")
     def get_text_from_tool_tips(self, hover_element, wait_element):
+        """
+        Retrieves the text from a tooltip by hovering over the specified element and waiting for the tooltip to appear.
+
+        Args:
+            hover_element: The element to hover over to display the tooltip.
+            wait_element: The element representing the tooltip content to wait for.
+
+        Returns:
+            str: The tooltip text.
+        """
         element = self.element_present(hover_element)
         self.go_to_element(element)
         self.action_move_to_element(element)
@@ -212,6 +308,12 @@ class ToolTipsPage(BasePage):
 
     @allure.step("check tool tips")
     def check_tool_tips(self):
+        """
+        Retrieves and returns the tooltip texts for various elements on the page.
+
+        Returns:
+            tuple: Tooltip texts for button, field, contrary section, and general section.
+        """
         tool_tip_text_button = self.get_text_from_tool_tips(
             self.locators.HOVER_ME_TO_SEE_BUTTON,
             self.locators.TOOL_TIP_BUTTON
@@ -240,6 +342,12 @@ class MenuItemPage(BasePage):
 
     @allure.step("check menu items")
     def check_menu_items(self):
+        """
+        Retrieves and returns the text of all menu items by hovering over them.
+
+        Returns:
+            list: A list of menu item texts.
+        """
         menu_items_list = self.elements_are_present(self.locators.MAIN_ITEM_LIST)
         data = []
         for item in menu_items_list:
@@ -253,6 +361,12 @@ class SelectMenuPage(BasePage):
 
     @allure.step("check click on select value items")
     def check_click_on_select_value_items(self):
+        """
+        Clicks on the drop-down and selects a random value from the options.
+
+        Returns:
+            str: The selected value.
+        """
         select_value = self.element_is_clickable(self.locators.SELECT_VALUE_DROP)
         select_value.click()
 
@@ -274,6 +388,12 @@ class SelectMenuPage(BasePage):
 
     @allure.step("check select on items")
     def check_select_one_items(self):
+        """
+         Clicks on the drop-down and selects a random title (e.g., 'Dr.', 'Mr.', etc.).
+
+        Returns:
+            str: The selected title.
+        """
         select_one = self.element_is_clickable(self.locators.SELECT_ONE_DROP)
         select_one.click()
         options = ["Dr.",
@@ -292,6 +412,12 @@ class SelectMenuPage(BasePage):
 
     @allure.step("check old style menu")
     def check_old_style_menu(self):
+        """
+        Selects a random color using the old-style select menu.
+
+        Returns:
+            tuple: The selected color and the displayed text.
+        """
         color_option = self.element_is_clickable(self.locators.SELECT_OLD_STYLE_DROP)
         color_option.click()
         color_input = random.sample(next(generated_color()).color_name, k=1)
@@ -301,6 +427,12 @@ class SelectMenuPage(BasePage):
 
     @allure.step("check multiselect drop down")
     def check_multiselect_drop_down(self):
+        """
+        Selects multiple options from the multi-select drop-down by sending the ENTER key multiple times.
+
+        Returns:
+            list: The filtered list of selected options.
+        """
         select_color = self.visible_element(self.locators.MULTISELECT_DROP)
         select_color.click()
         option_input = self.element_present(self.locators.MULTISELECT_DROP_OPTION)
@@ -316,5 +448,11 @@ class SelectMenuPage(BasePage):
 
     @allure.step("check standard multi select")
     def check_standard_multi_select(self):
+        """
+        Retrieves the text of the selected options in the standard multi-select.
+
+        Returns:
+            str: The selected options as a single string.
+        """
         multi_select = self.visible_element(self.locators.STANDARD_MULTI_SELECT)
         return multi_select.text.replace('\n', " ")
